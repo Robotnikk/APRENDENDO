7de6720ff151f31c6f128825af774090a296171b
Guilherme Rodrigues <guilherme.rodrigues2@live.com>
2021-12-22 10:44:03 -0300

    Set e Get
    Aula importante, necessario repetir aulas sobre
    construtor do objeto e setter e getter para
    melhor compreensão.

--- Robotnik/src/modulo9/CaracteristicasDosObjetos.java
+++ Robotnik/src/modulo9/CaracteristicasDosObjetos.java
@@ -1,7 +1,7 @@
 package modulo9;
 
 
-/* Parte da aula 9.7 , 9.8 , 9.9*/
+/* Parte da aula 9.7 , 9.8 , 9.9, 9.10*/
 
 public class CaracteristicasDosObjetos {
 	
@@ -20,13 +20,43 @@ public class CaracteristicasDosObjetos {
 		/* aluno1 é uma ref para o objeto PrimeiraClasseObjAluno*/
 		
 		PrimeiraClasseObjAluno aluno1 = new PrimeiraClasseObjAluno(); /* Futuro John */
-		aluno1.nome = "John";
-		aluno1.idade = 19;
+		aluno1.setNome("John do Future");
+		aluno1.setIdade(22);
+		aluno1.setDataMatricula("30/10/1993");
+		aluno1.setDataNascimento("30/10/1993");
+		aluno1.setNomeEscola("Sesi");
+		aluno1.setNomeMae("Tifa");
+		aluno1.setNomePai("Cloud");
+		aluno1.setTemCachorro(true);
+		aluno1.setRegistroGeral("20838364");
 		
-		System.out.println("Nome do aluno é: " + aluno1.nome);
-		System.out.println("Ele tem " + aluno1.idade + " anos");
+		
+		
+		System.out.println("Nome do aluno é: " + aluno1.getNome());
+		System.out.println("Ele tem " + aluno1.getIdade() + " anos");
+		System.out.println("Matriculado em " + aluno1.getDataMatricula());
+		System.out.println("Nasceu em " + aluno1.getDataNascimento());
+		System.out.println("Estudou na escola " + aluno1.getNomeEscola() );
+		System.out.println("Nome Mãe é " + aluno1.getNomeMae());
+		System.out.println("Nome Pai é " + aluno1.getNomePai());
+		System.out.println("RG dele é " + aluno1.getRegistroGeral());
+		
+		System.out.println("----------------------------------------------------------");
+		/*==================================================================================*/
 		
 		PrimeiraClasseObjAluno aluno2 = new PrimeiraClasseObjAluno(); /* Futura Wendy */
+		aluno2.setNome("Wendy a Garçonete");
+		aluno2.setIdade(24);
+		aluno2.setSerieMatriculado("3º ano");
+		aluno2.setNumeroCpf("00011155511");
+		
+		System.out.println(aluno2.getNome());
+		System.out.println(aluno2.getIdade());
+		System.out.println(aluno2.getSerieMatriculado());
+		System.out.println(aluno2.getNumeroCpf());
+		
+		
+		/*====================================================================================*/
 		
 		
 		PrimeiraClasseObjAluno aluno3 = new PrimeiraClasseObjAluno(); /* Futura Emily*/
@@ -44,5 +74,15 @@ public class CaracteristicasDosObjetos {
  		
 		
 	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
--- Robotnik/src/modulo9/PrimeiraClasseObjAluno.java
+++ Robotnik/src/modulo9/PrimeiraClasseObjAluno.java
@@ -2,7 +2,7 @@ package modulo9;
 
 /* classe / objeto que representa o aluno*/
 
-/*Aulas: 9.5 , 9.6 , 9.7 , 9.8*/
+/*Aulas: 9.5 , 9.6 , 9.7 , 9.8 , 9.9, 9.10 */
 public class PrimeiraClasseObjAluno {
 
 	
@@ -19,6 +19,9 @@ public class PrimeiraClasseObjAluno {
 	String serieMatriculado;
 	boolean temCachorro;
 
+	/*===================================================================================*/
+	
+	
 	public PrimeiraClasseObjAluno() { /* cria dados na memoria - padrão java */
 		// TODO Auto-generated constructor stub
 
@@ -40,6 +43,102 @@ public class PrimeiraClasseObjAluno {
 		idade = idadePadrao;
 		dataNascimento = dataNascimentoPadrao;
 	}
+	
+	/*Metodos setters(adicionar dados atrib) e getters(obter valores do atrib)*/
+	
+	
+	/*Recebe dados*/
+	public void setNome(String nome) {
+		this.nome = nome;
+		
+	}
+	
+	public String getNome() {
+		return this.nome;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+	public String getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(String dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public String getRegistroGeral() {
+		return registroGeral;
+	}
+
+	public void setRegistroGeral(String registroGeral) {
+		this.registroGeral = registroGeral;
+	}
+
+	public String getNumeroCpf() {
+		return numeroCpf;
+	}
+
+	public void setNumeroCpf(String numeroCpf) {
+		this.numeroCpf = numeroCpf;
+	}
+
+	public String getNomeMae() {
+		return nomeMae;
+	}
+
+	public void setNomeMae(String nomeMae) {
+		this.nomeMae = nomeMae;
+	}
+
+	public String getNomePai() {
+		return nomePai;
+	}
+
+	public void setNomePai(String nomePai) {
+		this.nomePai = nomePai;
+	}
+
+	public String getDataMatricula() {
+		return dataMatricula;
+	}
+
+	public void setDataMatricula(String dataMatricula) {
+		this.dataMatricula = dataMatricula;
+	}
+
+	public String getNomeEscola() {
+		return nomeEscola;
+	}
+
+	public void setNomeEscola(String nomeEscola) {
+		this.nomeEscola = nomeEscola;
+	}
+
+	public String getSerieMatriculado() {
+		return serieMatriculado;
+	}
+
+	public void setSerieMatriculado(String serieMatriculado) {
+		this.serieMatriculado = serieMatriculado;
+	}
+
+	public boolean isTemCachorro() {
+		return temCachorro;
+	}
+
+	public void setTemCachorro(boolean temCachorro) {
+		this.temCachorro = temCachorro;
+	}
+	
+	
+	
 	
 	
 	
